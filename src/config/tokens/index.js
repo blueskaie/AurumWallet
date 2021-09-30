d534f2b4f2123755076e8c7b5f10dabe1407b99e
robinAny <robin0224@mail.ru>
2021-09-30 13:07:24 -0400

    retry

--- src/config/tokens/index.js
+++ src/config/tokens/index.js
@@ -4,9 +4,8 @@ const BNB = require('./bnb.json')
 const BUSD = require('./busd.json')
 const USDT = require('./usdt.json')
 const ARU = require('./aru.json')
-const ADMONKEY = require('./admonkey.json')
 
-const ALL_TOKENS = [BNB,BUSD,ADMONKEY,ARU];
+const ALL_TOKENS = [BNB,BUSD,USDT,ARU];
 
 export const BNB_CODE = 'BNB';
 export const DEFAULT_TOKEN = BNB;
