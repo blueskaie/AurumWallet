dde8b7d89b78c804165a8940ed49950a0f014c3a
blueskaie <vanyantigran1992@gmail.com>
2021-10-22 16:54:44 +0800

    added permissions

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginTop: -40
   },
   mywallet: {
     marginTop: 10,
